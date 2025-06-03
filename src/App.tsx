44f6b6ebb3f2911ae358b7645947dfd8b9e63802
Mithun Yadav <mithuny1998@gmail.com>
2025-06-03 08:23:03 +0530

    ✨ feat: implement ProductsListing component and enhance ProductCard with add to cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Products from "./components/products/Products";
+import ProductsListing from "./components/productsLIsting/ProductsListing";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddToCart from "./components/addToCart/AddToCart";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux-toolkit/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      {/* <Cart /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/cart" element={<AddToCart />} />
-          {/* <Route path="/products" element={<Products />} /> */}
-          {/* <Route path="/products/:id" element={<div>Product Details</div>} /> */}
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          {/* <Cart /> */}
+          <ToastContainer position="bottom-right" autoClose={2000} />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<ProductsListing />} />
+              <Route path="/cart" element={<AddToCart />} />
+              {/* <Route path="/products" element={<Products />} /> */}
+              {/* <Route path="/products/:id" element={<div>Product Details</div>} /> */}
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
